cd068a9306845f6624dacfcc5d4bc97ed5fadfe1
drachodran <cesargayo@gmail.com>
2018-09-18 18:57:29 +0200

    Creada clase C21 según issue02, tercer commit

--- src/main/java/C11.java
+++ src/main/java/C11.java
@@ -1,7 +1,7 @@
 public class C11 {
     public String m1(){
-        String response1="response1";
-        return response1;
+       String response1="response1";
+       return response1;
     }
     public String m2(){
         String response2="response2";
