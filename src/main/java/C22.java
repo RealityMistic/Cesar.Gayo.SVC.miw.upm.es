5f0bfc783d70a649fc027dd7076fb3ea69c3a984
drachodran <cesargayo@gmail.com>
2018-09-18 22:38:10 +0200

    Clase C22 con método mA()

---
+++ src/main/java/C22.java
@@ -0,0 +1,6 @@
+public class C22 {
+    String mA(String param_to_append1, String param_to_append2){
+        String appendedParameters;
+        return appendedParameters = param_to_append1 + " " + param_to_append2;
+    }
+}
