27b055978149ba897982712754180b40d755ec10
drachodran <cesargayo@gmail.com>
2018-09-24 01:18:07 +0200

    .travis.yml

--- src/main/java/C52.java
+++ src/main/java/C52.java
@@ -1,6 +1,5 @@
 public class C52 {
     public String mB(String param1, String param2){
-        String result = param1 + "*" + param2;
-        return result;
+        return param1 + "*" + param2;
     }
 }
