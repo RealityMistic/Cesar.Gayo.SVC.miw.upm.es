ceeb7cd6404569a4c6650239e496f427f62586d1
drachodran <cesargayo@gmail.com>
2018-09-23 22:10:49 +0200

    arreglado el test C31Test

--- src/main/java/C51.java
+++ src/main/java/C51.java
@@ -1,3 +1,10 @@
 public class C51 {
-    
+    public String m1(){
+        String response1="response1-C51";
+        return response1;
+    }
+    public String m2(){
+        String response2="response2-C51";
+        return response2;
+    }
 }
--- src/test/java/C31Test.java
+++ src/test/java/C31Test.java
@@ -8,15 +8,15 @@ public class C31Test  {
         int testparam1, testparam2;
         testparam1 = 5;
         testparam2 = 3;
-        assertEquals(c31_to_test.m1(testparam1, testparam2), 15);
+        assertEquals(c31_to_test.m1(testparam1, testparam2), 8);
         }
     @Test
     public void m2Test(){
         C31 c31_to_test = new C31();
         float testparam1, testparam2;
-        testparam1 = 5.2f;
+        testparam1 = 5.0f;
         testparam2 = 3.0f;
-        float expected_float =15.6f;
+        float expected_float =15.0f;
         assertEquals(c31_to_test.m2(testparam1, testparam2), expected_float, 0.002f);
     }
 }
