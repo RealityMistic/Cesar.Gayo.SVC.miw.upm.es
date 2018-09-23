aff71d8dcd263700e65e5ce6f2988a0ff45fc069
drachodran <cesargayo@gmail.com>
2018-09-23 18:38:12 +0200

    C11Test.java

--- src/test/java/C11Test.java
+++ src/test/java/C11Test.java
@@ -1,2 +1,17 @@
-public class C11Testgit  {
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class C11Test  {
+    @Test
+    public void m1Test() {
+        C11 C11toTest = new C11();
+        String response1tested = C11toTest.m1();
+        assertEquals(response1tested, "response1");
+    }
+    @Test
+    public void m2Test() {
+        C11 C11toTest = new C11();
+        String response2tested = C11toTest.m2();
+       assertEquals(response2tested, "response2");
+    }
 }
